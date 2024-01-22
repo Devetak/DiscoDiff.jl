096c0c19b457a6027d5dccb9033ae7c125a38426
Mitja Devetak <m888itja@gmail.com>
2024-01-22 17:02:52 +0100

    website

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,9 @@
 using Documenter
 using DiscoDiff
 
-makedocs(sitename = "DiscoDiff.jl Documentation", modules = [DiscoDiff])
+makedocs(
+    sitename = "DiscoDiff.jl Documentation",
+    modules = [DiscoDiff],
+    format = Documenter.HTML(pretyurls = false),
+    pages = ["Home" => "index.md"],
+)
